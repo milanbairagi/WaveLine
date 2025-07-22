1366f6f12f8b64158753656ad4ddcfb07c4dc54f
NoName3755 <nmilan626@gmail.com>
2025-07-22 18:17:54 +0545

    feat: implement user creation with JWT token response

--- Backend/accounts/views.py
+++ Backend/accounts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from .serializers import UserSerializer
 from .models import User
@@ -10,3 +11,15 @@ class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny,]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        refresh = RefreshToken.for_user(user)
+        print(f"{refresh=}")
+        data = serializer.data
+        data["refresh"] = str(refresh)
+        data["access"] = str(refresh.access_token)
+        headers = self.get_success_headers(serializer.data)
+        return Response(data, status=status.HTTP_201_CREATED, headers=headers)
