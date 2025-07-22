88bc74930a6248728c9846e5cdfc4513efc47a02
NoName3755 <nmilan626@gmail.com>
2025-07-22 15:27:16 +0545

    feat: add user model in accounts app

--- Backend/core/settings.py
+++ Backend/core/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     "accounts",
 
-    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -123,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "accounts.User"
