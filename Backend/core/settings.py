370802c80a04f151cc888bc7baef7b867745f86e
NoName3755 <nmilan626@gmail.com>
2025-07-25 19:19:49 +0545

    feat(jwt): increase access and refresh token lifetime

--- Backend/core/settings.py
+++ Backend/core/settings.py
@@ -114,8 +114,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=10),
 }
 
 
