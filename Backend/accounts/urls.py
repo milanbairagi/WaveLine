1202da7ad38ffe958da89cadfb18a3fdf46e63f3
NoName3755 <nmilan626@gmail.com>
2025-07-25 23:14:07 +0545

    feat(api): create user retrieve view and change accounts app's url
    The url for accounts app prefix change from /api/ to /api/accounts/ and
    create a new view `RetrieveUserView` that returns currently
    authenticated user info.

--- Backend/accounts/urls.py
+++ Backend/accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CreateUserView
+from .views import CreateUserView, RetrieveUserView
 
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="register"),
+    path("user/", RetrieveUserView.as_view(), name="get_user"),
 ]
--- Backend/accounts/views.py
+++ Backend/accounts/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics, status
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 
@@ -17,9 +17,17 @@ class CreateUserView(generics.CreateAPIView):
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
         refresh = RefreshToken.for_user(user)
-        print(f"{refresh=}")
         data = serializer.data
         data["refresh"] = str(refresh)
         data["access"] = str(refresh.access_token)
         headers = self.get_success_headers(serializer.data)
         return Response(data, status=status.HTTP_201_CREATED, headers=headers)
+
+
+class RetrieveUserView(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated,]
+    
+    def get_object(self):
+        return self.request.user
