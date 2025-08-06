d92781c1a64c34e3270a3308933b16625ed3ec3e
NoName3755 <nmilan626@gmail.com>
2025-08-06 18:51:32 +0545

    refractor: make tab spaces 2

--- Frontend/src/components/buttons/Logout.jsx
+++ Frontend/src/components/buttons/Logout.jsx
@@ -1,20 +1,16 @@
-import { useUser } from "../../context/userContext"
+import { useUser } from "../../context/userContext";
 import { useNavigate } from "react-router-dom";
 
 const Logout = () => {
-    const { logoutUser } = useUser();
-    const navigate = useNavigate();
+	const { logoutUser } = useUser();
+	const navigate = useNavigate();
 
-    const handleLogout = () => {
-        logoutUser();
-        navigate("/login");
-    };
+	const handleLogout = () => {
+		logoutUser();
+		navigate("/login");
+	};
 
-    return (
-        <button onClick={handleLogout}>
-            Logout
-        </button>
-    );
+	return <button onClick={handleLogout}>Logout</button>;
 };
 
 export default Logout;
